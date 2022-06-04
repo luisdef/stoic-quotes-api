cc09b56fa363cd860d11c0bee03bdd282675610e
luisdef <luis.assmann.1234@gmail.com>
2022-06-04 13:36:08 -0300

    CORS

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ function getQuote() {
     return data[indexOfSelectedQuote];
 }
 
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET');
+    app.use(cors());
+    next();
+});
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/info.html"));
 });
@@ -27,4 +36,4 @@ app.listen(port, () => {
     console.info("Application running on https://localhost:"+port);
 });
 
-getQuote();
+// console.log(getQuote());
