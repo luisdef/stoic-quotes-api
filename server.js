03e761a3a8e7795c36d17c94cabb0f5e5166420c
luisdef <luis.assmann.1234@gmail.com>
2022-06-04 12:39:38 -0300

    Creating the routes

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+const { application } = require("express");
 const express = require("express");
 const app = express();
+const data = require("./quotes.json");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 
+
+
+function getQuote() {
+    // This function returns a random quote from the JS Object.
+    let indexOfSelectedQuote = Math.floor(Math.random() * data.length);
+    
+    return data[indexOfSelectedQuote];
+}
+
 app.get("/", (req, res) => {
-    res.json({
-        "quote": "You become what you give your attention to.",
-        "author": "Epictetus"
-    });
+    res.sendFile(path.join(__dirname, "/info.html"));
+});
+
+app.get("/api/quote/random", (req, res) => {
+    const randomQuote = getQuote();
+    res.json(randomQuote);
 });
 
 app.listen(port, () => {
     console.info("Application running on https://localhost:"+port);
 });
+
+getQuote();
