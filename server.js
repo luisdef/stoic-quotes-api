25125bd13c6e8197a1c162791b6b83fb9ce31692
luisdef <luis.assmann.1234@gmail.com>
2022-06-08 20:12:19 -0300

    Static folder added

--- server.js
+++ server.js
@@ -6,35 +6,34 @@ const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 
-
-
-function getQuote() {
-    // This function returns a random quote from the JS Object.
-    let indexOfSelectedQuote = Math.floor(Math.random() * data.length);
-    
-    return data[indexOfSelectedQuote];
-}
+// Define a pasta root (para as instruções por /info/info.html)
+app.use(express.static(path.join(__dirname, "info")));
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET');
-    app.use(cors());
-    next();
+  // Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+
+  // Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", 'GET');
+
+  app.use(cors());
+  next();
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/info.html"));
+  res.sendFile(path.join(__dirname, "/info/info.html"));
 });
 
+function getQuote() {
+  let indexOfSelectedQuote = Math.floor(Math.random() * data.length);
+  return data[indexOfSelectedQuote];
+}
+
 app.get("/api/quote/random", (req, res) => {
-    const randomQuote = getQuote();
-    res.json(randomQuote);
+  const randomQuote = getQuote();
+  res.json(randomQuote);
 });
 
 app.listen(port, () => {
-    console.info("Application running on https://localhost:"+port);
+  console.info("Aplicação rodando na porta " + port);
 });
-
-// console.log(getQuote());
