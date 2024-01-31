c4ee1457650fbfbf149e5acb77a98c86d90443a5
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-01-31 15:44:07 -0300

    get from db

---
+++ database.js
@@ -0,0 +1,31 @@
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER_DB,
+  password: process.env.PASSWD_DB,
+  database: process.env.DATABASE,
+});
+
+async function getRandomQuote() {
+  try {
+    const [results] = await connection.query('SELECT COUNT(1) as nfrases FROM frases;');
+    const totalLength = results[0]['nfrases'];
+    let indexOfSelectedQuote = Math.floor(Math.random() * totalLength);
+  
+    try {
+      const [results] = await connection.query(`SELECT * FROM frases WHERE idFrase = ${indexOfSelectedQuote}`);
+  
+      return {
+        "frase": results[0]["frase"],
+        "autor": results[0]["autor"]
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export { getRandomQuote };
