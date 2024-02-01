5e7ef364e54c17a24c6640b8179fb343d57821ab
Luis Felipe Assmann <59901281+luisdef@users.noreply.github.com>
2024-02-01 15:29:45 -0300

    update to host db

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
-  host: process.env.HOST,
+  host: process.env.HOST_DB,
   user: process.env.USER_DB,
   password: process.env.PASSWD_DB,
   database: process.env.DATABASE,
